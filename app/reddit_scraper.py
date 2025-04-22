ff7415983c7d2f8fe03463042568f5153dd87eed
Cezary Suchorski <cezary.suchorski@gmail.com>
2025-04-22 23:04:41 +0200

    Added a RedditScraper class that connects to a reddit instance and allows to fetch posts/comments

--- app/reddit_scraper.py
+++ app/reddit_scraper.py
@@ -2,13 +2,43 @@ import praw
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
 
-reddit = praw.Reddit(
-    client_id=os.getenv("REDDIT_CLIENT_ID"),
-    client_secret=os.getenv("REDDIT_CLIENT_SECRET"),
-    user_agent=os.getenv("REDDIT_USER_AGENT")
-)
+class RedditScraper:
+    def __init__(self):
+        load_dotenv()
+        self.reddit = self._create_reddit_instance()
 
-for post in reddit.subreddit("technology").hot(limit=5):
-    print(post.title)
+    def _create_reddit_instance(self):
+        reddit = praw.Reddit(
+            client_id=os.getenv("REDDIT_CLIENT_ID"),
+            client_secret=os.getenv("REDDIT_CLIENT_SECRET"),
+            user_agent=os.getenv("REDDIT_USER_AGENT")
+        )
+
+        return reddit
+
+    def fetch_posts(self, subreddit_name, limit=5):
+        subreddit = self.reddit.subreddit(subreddit_name)
+        posts = []
+        for post in subreddit.hot(limit=limit):
+            posts.append({
+                "id": post.id,
+                "title": post.title,
+                "body": post.selftext,
+                "created_at": post.created_utc
+            })
+
+        return posts
+
+    def fetch_comments_for_post(self, post):
+        post.comments.replace_more(limit=0)
+        comments = []
+        for comment in post.comments.list():
+            comments.append({
+                "id": comment.id,
+                "body": comment.body,
+                "score": comment.score,
+                "created_at": comment.created_utc
+            })
+
+        return comments
