42dc46b766be5df0e3309f64072aec4d81dc0f11
Cezary Suchorski <cezary.suchorski@gmail.com>
2025-04-29 10:17:16 +0200

    Created a PostList component to display post analysis results

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,17 @@
 import { useState } from "react";
 import SubredditForm from "./components/SubredditForm";
 import "./App.css";
+import PostList from "./components/PostList";
 
 function App() {
   const [subredditData, setSubredditData] = useState(null);
 
   const analyzeSubreddit = async (subreddit, postLimit, commentLimit) => {
     try {
-      const response = fetch(
+      const response = await fetch(
         `http://localhost:8000/analyze?subreddit=${subreddit}&posts=${postLimit}&comments=${commentLimit}`
       );
-      const data = (await response).json();
+      const data = await response.json();
       setSubredditData(data);
     } catch (error) {
       console.error(error);
@@ -21,6 +22,7 @@ function App() {
     <div>
       <h1>Subreddit Sentiment Analyzer</h1>
       <SubredditForm onSubmit={analyzeSubreddit} />
+      {subredditData && <PostList postData={subredditData} />}
     </div>
   );
 }
