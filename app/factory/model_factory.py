fc35b085c34a7c273776918f7d53544e25a56659
Cezary Suchorski <cezary.suchorski@gmail.com>
2025-05-10 11:18:41 +0200

    Refactored ModelFactory to use model caching, to reduce the constant model reinitialization; Changed the create_model function to use a match/case instead of else ifs

--- app/factory/model_factory.py
+++ app/factory/model_factory.py
@@ -1,11 +1,27 @@
+from app.models.roberta_model import RobertaModel
+from app.models.text_blob_model import TextBlobModel
 from app.models.vader_model import VaderModel
 
 
 class ModelFactory():
+    _model_cache = {}
+
     @staticmethod
     def create_model(model_type):
         model_type = model_type.lower()
-        if model_type == "vader":
-            return VaderModel()
-        else:
-            raise ValueError(f"Unknown model type: {model_type}")
+
+        if model_type in ModelFactory._model_cache:
+            return ModelFactory._model_cache[model_type]
+
+        match model_type:
+            case "vader":
+                model = VaderModel()
+            case "roberta":
+                model = RobertaModel()
+            case "textblob":
+                model = TextBlobModel()
+            case _:
+                raise ValueError(f"Unknown model type: {model_type}")
+
+        ModelFactory._model_cache[model_type] = model
+        return model
