48abd5c264763bb3df1c15e8d79553e07bfb5a5c
Cezary Suchorski <cezary.suchorski@gmail.com>
2025-05-14 19:55:11 +0200

    Added donut charts to the summary, styling changes to the report

--- frontend/src/components/Summary.jsx
+++ frontend/src/components/Summary.jsx
@@ -1,10 +1,24 @@
+import "../styles/Summary.css";
+import MeanDonutChart from "./MeanDonutChart";
+
 function Summary({ meanTitleScore, meanBodyScore, meanCommentScore }) {
   return (
     <div className="summary">
       <h2>Sentiment Summary</h2>
-      <p>Mean Title Score: {meanTitleScore.toFixed(2)}</p>
-      <p>Mean Body Score: {meanBodyScore.toFixed(2)}</p>
-      <p>Mean Comment Score: {meanCommentScore.toFixed(2)}</p>
+      <div className="chart-container">
+        <span className="donut-chart">
+          <MeanDonutChart label="positive" score={meanTitleScore} />
+          Mean Title Score: {meanTitleScore.toFixed(2)}
+        </span>
+        <span className="donut-chart">
+          <MeanDonutChart label="neutral" score={meanBodyScore} />
+          Mean Body Score: {meanBodyScore.toFixed(2)}
+        </span>{" "}
+        <span className="donut-chart">
+          <MeanDonutChart label="negative" score={meanCommentScore} />
+          Mean Comment Score: {meanCommentScore.toFixed(2)}
+        </span>
+      </div>
     </div>
   );
 }
