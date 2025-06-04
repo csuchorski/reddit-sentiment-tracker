c1cf54fba6d9d338dd2a13b5552b9eb0c91eca6f
Cezary Suchorski <cezary.suchorski@gmail.com>
2025-06-04 10:01:46 +0200

    Added unit tests for the model factory, changed error message inside the factory for invalid model name

--- app/factory/model_factory.py
+++ app/factory/model_factory.py
@@ -21,7 +21,7 @@ class ModelFactory():
             case "textblob":
                 model = TextBlobModel()
             case _:
-                raise ValueError(f"Unknown model type: {model_type}")
+                raise ValueError(f"Invalid model type: {model_type}")
 
         ModelFactory._model_cache[model_type] = model
         return model
